a83ce4317e54784e26802669cee07a568c88ddaa
Joy <jtkaufman737@gmail.com>
2018-03-24 09:15:04 -0400

    Checking scraper still works, wrapping functionality into method

--- main.py
+++ main.py
@@ -14,34 +14,35 @@ z=0
 y=2
 w=0
 
-# -- do work --
-for z in range(0,20): #handle statically
-    r = requests.get('http://www.yangtse.com/app/health/')
-    soup = BeautifulSoup(r.text,'html.parser')
-    newsitems = soup.find_all('div',attrs={'class':'box'})
-    curr_newsitem = newsitems[x]
-    title=curr_newsitem.find('div',{'class':'box-text-title'}).text
-    link=curr_newsitem.find('a')['href']
-    date=curr_newsitem.find('div',{'class':'box-text-time'}).text
-    articles.append((title,link,date))
-    z+=1
-    x+=1
-for z in range(21,101):
-    stry=str(y)  #handle somewhat dynamically
-    r = requests.get('http://www.yangtse.com/app/health/index_'+ stry + '.html')
-    soup = BeautifulSoup(r.text,'html.parser')
-    newsitems = soup.find_all('div',attrs={'class':'box'})
-    curr_newsitem = newsitems[w]
-    title=curr_newsitem.find('div',{'class':'box-text-title'}).text
-    link=curr_newsitem.find('a')['href']
-    date=curr_newsitem.find('div',{'class':'box-text-time'}).text
-    articles.append((title,link,date))
-    z+=1
-    w+=1
-    if w%20==0:
-        w-=20
-        y+=1
+def scrape:
+    # -- do work --
+    for z in range(0,20): #handle statically
+        r = requests.get('http://www.yangtse.com/app/health/')
+        soup = BeautifulSoup(r.text,'html.parser')
+        newsitems = soup.find_all('div',attrs={'class':'box'})
+        curr_newsitem = newsitems[x]
+        title=curr_newsitem.find('div',{'class':'box-text-title'}).text
+        link=curr_newsitem.find('a')['href']
+        date=curr_newsitem.find('div',{'class':'box-text-time'}).text
+        articles.append((title,link,date))
         z+=1
+        x+=1
+    for z in range(21,101):
+        stry=str(y)  #handle somewhat dynamically
+        r = requests.get('http://www.yangtse.com/app/health/index_'+ stry + '.html')
+        soup = BeautifulSoup(r.text,'html.parser')
+        newsitems = soup.find_all('div',attrs={'class':'box'})
+        curr_newsitem = newsitems[w]
+        title=curr_newsitem.find('div',{'class':'box-text-title'}).text
+        link=curr_newsitem.find('a')['href']
+        date=curr_newsitem.find('div',{'class':'box-text-time'}).text
+        articles.append((title,link,date))
+        z+=1
+        w+=1
+        if w%20==0:
+            w-=20
+            y+=1
+            z+=1
 
 
 extra_extra = pd.DataFrame(articles,columns=['Title','Link','Date'])
